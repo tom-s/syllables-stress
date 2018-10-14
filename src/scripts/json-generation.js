e32b2da6ba9fa1d6eddc88272a70337fe595b74a
tom-s <thom.schell@gmail.com>
2018-10-15 00:14:42 +0200

    better generation

--- src/scripts/json-generation.js
+++ src/scripts/json-generation.js
@@ -1,27 +1,45 @@
+const fs = require('fs')
+const splitHtml = require('split-html')
+const compact = require('lodash/compact')
+const isEmpty = require('lodash/isEmpty')
 const PATH = 'src/resources/stresses/'
 const JSON_PATH = 'src/resources/stresses/json/'
-const fs = require('fs')
 
-const data = fs.readFileSync(`${PATH}stresses.txt`)
+
 let letterResult = {}
 let currentLetter = null
 let fullResult = {}
+
+const data = fs.readFileSync(`${PATH}stresses.txt`)
 const splitList = data.toString().split(/\n/)
 
+const DEBUG_START = 45220 //letter H
+
 for (var i = 0; i < splitList.length; i++) {
+//for (var i = DEBUG_START; i < DEBUG_START + 10; i++) { //for debug
   const line = splitList[i].split(' 	')
   const word = line[0]
   const stresses = line[1]
   const firstLetter = word.charAt(0)
-  if(currentLetter && firstLetter !== currentLetter) {
+  if(currentLetter && firstLetter !== currentLetter && !isEmpty(letterResult)) {
     // Write previous file
     const fileLetterContent = JSON.stringify(letterResult)
     fs.writeFileSync(`${JSON_PATH}${currentLetter}.json`, fileLetterContent)
     letterResult = {}
   }
-  currentLetter = firstLetter
-  letterResult[word] = stresses
-  fullResult[word] = stresses
+  const fragments = compact(splitHtml(stresses, 'b').filter(frag => frag !== '<b></b>'))
+   // Only add words with several syllables
+   if(fragments.length > 1) {
+    const stressesArray = fragments.map(frag => {
+      return {
+        syllable: frag.replace('<b>', '').replace('</b>', ''),
+        primaryStress: frag.includes('<b>') || fragments.length === 1
+      }
+    })
+    currentLetter = firstLetter
+    letterResult[word] = stressesArray
+    fullResult[word] = stressesArray
+  }
 }
 
 // Write last letter file
